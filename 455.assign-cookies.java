5383b56f4368e0aab0a38e75b277181da0efc01c
Farhan-Islam-Rafid <rafidfarhanislam@gmail.com>
2026-07-02 13:41:27 +0600

    done

---
+++ 455.assign-cookies.java
@@ -0,0 +1,34 @@
+
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=455 lang=java
+ *
+ * [455] Assign Cookies
+ */
+
+// @lc code=start
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+
+        // g = baby , s = cookies
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0, j = 0;
+
+        while (i < g.length && j < s.length) {
+
+            if (s[j] >= g[i]) {
+
+                i++;
+            }
+            j++;
+
+        }
+
+        return i;
+    }
+}
+// @lc code=end
