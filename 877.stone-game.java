f6a595e8255930c55832fd2110492ccd57feecb0
Farhan-Islam-Rafid <rafidfarhanislam@gmail.com>
2026-08-02 14:36:08 +0600

    877 done

---
+++ 877.stone-game.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=877 lang=java
+ *
+ * [877] Stone Game
+ */
+
+// @lc code=start
+class Solution {
+
+    public boolean stoneGame(int[] piles) {
+
+        return true; // karon alice sob somoe jetbe  karon je first a sujok pae 
+
+    }
+}
+// @lc code=end
+
