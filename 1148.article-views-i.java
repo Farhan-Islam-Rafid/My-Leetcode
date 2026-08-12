5a75cc4915df9e38779ef897d219591e3eb2c2a7
Farhan-Islam-Rafid <rafidfarhanislam@gmail.com>
2026-08-12 10:50:08 +0600

    done database prob

---
+++ 1148.article-views-i.java
@@ -0,0 +1,4 @@
+select email #email dekbo
+from person #person table theke
+group by email #proti ti email grp korbo mane koybae same email ase ta count korbo 
+having count(email) > 1; # 1 tir bese same email thakle ta dekhabee
