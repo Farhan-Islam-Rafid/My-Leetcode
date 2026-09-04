e87169bca8e844fc1b599189fef3c620d3703886
Farhan-Islam-Rafid <rafidfarhanislam@gmail.com>
2026-09-04 12:12:32 +0600

    done

---
+++ 3903.smallest-stable-index-i.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=3903 lang=java
+ *
+ * [3903] Smallest Stable Index I
+ */
+
+// @lc code=start
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+
+         int n = nums.length;
+
+        int[] max = new int[n];
+        int[] min = new int[n];
+
+        max[0] = nums[0];
+
+        for (int i = 1; i < n; i++)
+            max[i] = Math.max(max[i - 1], nums[i]);
+
+        min[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--)
+            min[i] = Math.min(min[i + 1], nums[i]);
+
+        for (int i = 0; i < n; i++) {
+            if (max[i] - min[i] <= k)
+                return i;
+        }
+
+        return -1;
+        
+    }
+}
+// @lc code=end
+
