5f39ae3692a26e5ac9bfdf1b06e94d170ebdfca7
Farhan-Islam-Rafid <rafidfarhanislam@gmail.com>
2026-09-08 13:56:37 +0600

    done

---
+++ 3870.count-commas-in-range.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=3870 lang=java
+ *
+ * [3870] Count Commas in Range
+ */
+
+// @lc code=start
+class Solution {
+    public int countCommas(int n) {
+
+            return Math.max(0, n - 999); 
+        
+    }
+}
+// @lc code=end
+
