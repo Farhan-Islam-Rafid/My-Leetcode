bbbcc61bfd02a3f73314a6d119c51ad6fad441af
Farhan-Islam-Rafid <rafidfarhanislam@gmail.com>
2026-09-13 17:53:28 +0600

    done

---
+++ 706.design-hash-map.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=706 lang=java
+ *
+ * [706] Design HashMap
+ */
+
+// @lc code=start
+import java.util.Arrays;
+
+class MyHashMap {
+
+    int[] map = new int[1000001];
+
+    public MyHashMap() {
+
+        Arrays.fill(map, -1);
+
+    }
+
+    public void put(int key, int value) {
+
+        map[key] = value;
+
+    }
+
+    public int get(int key) {
+
+        return map[key];
+
+    }
+
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such: MyHashMap obj
+ * = new MyHashMap(); obj.put(key,value); int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
+// @lc code=end
+
