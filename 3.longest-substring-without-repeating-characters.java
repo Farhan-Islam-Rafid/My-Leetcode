0e023ab7b085ba91776f923cc51ac140c956bb55
Farhan-Islam-Rafid <rafidfarhanislam@gmail.com>
2026-09-15 12:07:59 +0600

    done

---
+++ 3.longest-substring-without-repeating-characters.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=3 lang=java
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+import java.util.HashSet;
+
+class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+
+                left++;
+
+            }
+
+            set.add(s.charAt(right));
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
+
