62b33b58ca9330f41375fb33a36fcd83f0d0e007
Farhan-Islam-Rafid <rafidfarhanislam@gmail.com>
2026-09-25 14:44:41 +0600

    done

---
+++ 561.array-partition.java
@@ -0,0 +1,27 @@
+
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=561 lang=java
+ *
+ * [561] Array Partition
+ */
+// @lc code=start
+class Solution {
+
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i += 2) {
+
+            sum += nums[i];
+        }
+
+        return sum;
+
+    }
+}
+// @lc code=end
+
